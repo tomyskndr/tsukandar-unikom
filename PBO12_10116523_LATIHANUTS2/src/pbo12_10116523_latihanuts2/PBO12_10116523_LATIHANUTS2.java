d4c9f675d2cf31708fdcf41637abad4f90817ecb
tomyskndr <32489340+tomyskndr@users.noreply.github.com>
2017-11-14 22:42:01 +0700

    Add files via upload

---
+++ PBO12_10116523_LATIHANUTS2/src/pbo12_10116523_latihanuts2/PBO12_10116523_LATIHANUTS2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo12_10116523_latihanuts2;
+
+import view.Second;
+
+/**
+ *
+ * @author user
+ */
+public class PBO12_10116523_LATIHANUTS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Second s = new Second();
+        s.setVisible(true);
+    }
+    
+}
